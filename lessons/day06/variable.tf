734807e4315ba06a8cf81c63d189683f18852de4
Sibananda Pradhan <anand7978085510@gmail.com>
2026-02-09 23:43:18 +0530

    Add Terraform variables for AWS resources
    Define variables for AWS resource configuration.

---
+++ lessons/day06/variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "The AWS region where resources will be created"
+  type    = string
+  default = "ap-south-1"
+
+}
+
+variable "channel_name" {
+  description = "this is for name pupose"
+  type = string
+  default = "siba-tf-bucket-2026"
+}
+variable "instance_type" {
+  description = "The type of EC2 instance to creaate"
+  type  = string
+  default = "t2.micro"
+}
+variable "environment" {
+  description = "The environment for which resources are being created"
+  type    = string
+  default = "dev"
+
+}
